9973050cc35046becff9f8e342064bf4c5a3854a
VictorDhein <victordhein@gmail.com>
2024-10-09 15:10:49 -0300

    arrumando o erro

--- src/variaveis/CastingSimulacao.java
+++ src/variaveis/CastingSimulacao.java
@@ -12,19 +12,31 @@ public class CastingSimulacao {
 
         // Casting Implícito
         System.out.print("Digite um número inteiro: ");
-        int numeroInteiro = scanner.nextInt(); // Leitura do número inteiro
+        int numInt = scanner.nextInt(); // Leitura do número inteiro
 
         // Conversão implícita de int para double
-        double numeroDouble = numeroInteiro;
-        System.out.println("Casting Implícito: " + numeroInteiro + " convertido para double: " + numeroDouble);
+        double numeroDouble = numInt;
+        System.out.println("Casting Implícito: " + numInt + " convertido para double: " + numeroDouble);
 
-        // Casting Explícito
-        System.out.print("Digite um número decimal: ");
-        double numeroDecimal = scanner.nextDouble(); // Leitura do número decimal
+        // Conversão de int para Integer (Wrapper)
+        Integer numeroInteiroWrapper = Integer.valueOf(numInt);
+        System.out.println("Conversão para Integer: " + numeroInteiroWrapper);
 
-        // Conversão explícita de double para int
-        int numeroConversao = (int) numeroDecimal; // Casting explícito
-        System.out.println("Casting Explícito: " + numeroDecimal + " convertido para int: " + numeroConversao);
+        // Conversão de long para int
+        System.out.print("Digite um número longo de até 11 digitos (long): ");
+        long numeroLong = scanner.nextLong(); // Leitura do número longo
+
+        // Conversão de long para int (casting explícito)
+        int numeroInt = (int) numeroLong; // O casting pode levar à perda de dados se o long for muito grande
+        System.out.println("Número longo " + numeroLong + " convertido para int: " + numeroInt);
+
+        // Conversão de int para short
+        System.out.print("Digite um número inteiro (int): ");
+        int num1 = scanner.nextInt(); // Leitura do número inteiro
+
+        // Conversão de int para short (casting explícito)
+        short numeroShort = (short) num1; // O casting pode levar à perda de dados se o int for muito grande
+        System.out.println("Número inteiro " + num1 + " convertido para short: " + numeroShort);
 
         // Fechar o scanner
         scanner.close();
--- src/variaveis/Variaveis.java
+++ src/variaveis/Variaveis.java
@@ -23,3 +23,6 @@ public class Variaveis {
     short s = b;
 
 }
+
+
+
